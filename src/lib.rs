3bd1528d531acd30993b076196976339f4e54449
peng.xi <peng.xi@rayz.ai>
2025-09-18 10:36:07 +0800

    update sdk to v0.33.5 and set weather as default

--- src/lib.rs
+++ src/lib.rs
@@ -475,7 +475,7 @@ fn pick_app_cli_params(params: Vec<HashMap<String, Value>>) -> Vec<String> {
         }
     }
     args.push("--weather".to_owned());
-    args.push("cloudy".to_owned());
+    args.push("sunny".to_owned());
     args.push("--no-imu".to_owned());
     args.push("--off".to_owned());
     args
